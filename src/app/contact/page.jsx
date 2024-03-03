6971778e0040f0b094af83324e3cbe17efd4fb16
Nitin Kumar <82951031+nitin-k6@users.noreply.github.com>
2024-03-03 15:56:18 +0530

    Update page.jsx

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -19,7 +19,7 @@ const sendEmail = (e) => {
 
     emailjs 
       .sendForm(process.env.NEXT_PUBLIC_SERVICE_ID , process.env.NEXT_PUBLIC_TEMPLATE_ID, form.current, {
-        publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+        publicKey:pCwzGVj2B2HhLWLaE,
       })
       .then(
         () => {
