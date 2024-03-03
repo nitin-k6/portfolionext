051f5f473c20c8a09102a44e0bcc39915e45c7c3
Nitin <67nitinkumar@gmail.com>
2024-03-03 18:54:38 +0530

    nextjsportfolio

--- loader.js
+++ loader.js
@@ -2,5 +2,5 @@
 
 export default function myImageLoader({src , width, quality}) {
     if(src.startsWith('https://images.pexels.com')) return src
-    return `https://nextjsportfolio.onrender.com/${src}?w=${width}&q=${quality || 75}`
+    return `nextjsportfolio2.vercel.app/${src}?w=${width}&q=${quality || 75}`
 }   
