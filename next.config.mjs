37cab8b0a6e7dbe72a5074658bafe0aaab1aaba1
Nitin <67nitinkumar@gmail.com>
2024-03-03 13:01:12 +0530

    nextjsportfolio

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+      loader: "custom", // for deployment   this is to load images from external source (in this case we have pexels). Also we  to make loader.js file 
+      loaderFile: "./loader.js", // for deployment
       remotePatterns: [
         {
           protocol: "https",
